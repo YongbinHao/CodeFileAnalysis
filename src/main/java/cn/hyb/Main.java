51c42070ddb44b9f82a28dd5d2e9ec12ae3629dc
845415849@qq.com <githubhyb123>
2021-07-30 16:46:31 +0800

    优化输入及提示信息

--- src/main/java/cn/hyb/Main.java
+++ src/main/java/cn/hyb/Main.java
@@ -1,6 +1,8 @@
 package cn.hyb;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,15 +14,25 @@ public class Main {
      * 代码扫描路径
      */
     static String BASE_WORKSPACE = "";//C:\Users\84541\Desktop\calculater
+    static String TARGET_SUFFIX = ".java";
 
     public static void main(String[] args) {
-
+        //输入信息
         System.out.println("请输入目标代码库地址：（如 C:\\Users\\84541\\Desktop\\calculater）");
         Scanner scanner = new Scanner(System.in);
         BASE_WORKSPACE = scanner.nextLine();
+        Path path = Paths.get(BASE_WORKSPACE);
+        if (!Files.exists(path) || !Files.isDirectory(path)) {
+            System.out.println("目录不存在或不为目录");
+            return;
+        }
+        System.out.println("请输入要扫描的文件后缀名，以空格分割，默认只扫描.java文件");
+        String inputSuffix = scanner.nextLine();
+        TARGET_SUFFIX = "".equals(inputSuffix) ? TARGET_SUFFIX : inputSuffix;
         System.out.println("开始扫描代码库，请等待");
+
         //分析目录
-        Map<Path, HashMap<String, String>> fileAnalysisRes = AnalyseUtil.analysis(BASE_WORKSPACE, Arrays.asList(".java", ".xml"));
+        Map<Path, HashMap<String, String>> fileAnalysisRes = AnalyseUtil.analysis(BASE_WORKSPACE, Arrays.asList(TARGET_SUFFIX.split(" ")));
         //汇总统计
         AtomicReference<Integer> totalLineNum = new AtomicReference<>(0);
         AtomicReference<Integer> totalNoteLineNum = new AtomicReference<>(0);
